713dd6be471159d871bf2f2d3a759bfe2fdc7902
Chris Feijoo <christophe@feijoo.fr>
2016-10-13 18:39:31 +0200

    Format Code

--- src/index.ts
+++ src/index.ts
@@ -42,23 +42,30 @@ export const get = perform('GET')
 export const post = perform('POST')
 export const patch = perform('PATCH')
 
-export const getUsers = (api: Connection, page: number): Promise<ApiUsers> =>
-  get(api, `users?page[number]=${page}`)
+export const getUsers =
+  (api: Connection, page: number): Promise<ApiUsers> =>
+    get(api, `users?page[number]=${page}`)
 
-export const getUser = (api: Connection, userId: number): Promise<ApiUser> =>
-  get(api, `users/${userId}`)
+export const getUser =
+  (api: Connection, userId: number): Promise<ApiUser> =>
+    get(api, `users/${userId}`)
 
-export const getProjects = (api: Connection, page: number): Promise<ApiProjects> =>
-  get(api, `projects?page[number]=${page}`)
+export const getProjects =
+  (api: Connection, page: number): Promise<ApiProjects> =>
+    get(api, `projects?page[number]=${page}`)
 
-export const getProject = (api: Connection, projectId: number): Promise<ApiProject> =>
-  get(api, `projects/${projectId}`)
+export const getProject =
+  (api: Connection, projectId: number): Promise<ApiProject> =>
+    get(api, `projects/${projectId}`)
 
-export const getLocations = (api: Connection): Promise<ApiLocations> =>
-  get(api, `locations`)
+export const getLocations =
+  (api: Connection): Promise<ApiLocations> =>
+    get(api, `locations`)
 
-export const getUserLocations = (api: Connection, userId: number): Promise<ApiLocations> =>
-  get(api, `locations/${userId}`)
+export const getUserLocations =
+  (api: Connection, userId: number): Promise<ApiLocations> =>
+    get(api, `locations/${userId}`)
 
-export const getCampusLocations = (api: Connection, campudId: number): Promise<ApiLocations> =>
-  get(api, `locations/${campudId}`)
+export const getCampusLocations =
+  (api: Connection, campudId: number): Promise<ApiLocations> =>
+    get(api, `locations/${campudId}`)
