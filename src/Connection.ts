7a034ccbbde51744117fb8a73c302e18b3299b4a
Chris Feijoo <christophe@feijoo.fr>
2016-10-15 15:33:07 +0200

    Simplify Connection

--- src/Connection.ts
+++ src/Connection.ts
@@ -10,33 +10,21 @@
 
 import { fetchToken } from './fetchToken'
 
-export type Config = {
-  protocol: string,
-  hostName: string,
-  tokenURL: string,
-  rootEndPoint: string,
-  clientId: string,
-  clientSecret: string
-}
-
 /**
  * Builds a connection to 42 api
  */
 export class Connection {
-  server: string
-  entryPoint: string
-  tokenUrl: string
+  server: string = 'https://api.intra.42.fr/'
+  entryPoint: string = 'https://api.intra.42.fr/v2/'
+  tokenUrl: string = 'https://api.intra.42.fr/oauth/token'
   clientId: string
   clientSecret: string
 
   private currentToken: string
 
-  constructor(config: Config) {
-    this.server = `${config.protocol}://${config.hostName}`
-    this.entryPoint = `${config.protocol}://${config.hostName}${config.rootEndPoint}`
-    this.tokenUrl = `${config.protocol}://${config.hostName}${config.tokenURL}`
-    this.clientId = config.clientId
-    this.clientSecret = config.clientSecret
+  constructor(clientId: string, clientSecret: string) {
+    this.clientId = clientId
+    this.clientSecret = clientSecret
   }
 
   getToken() {
